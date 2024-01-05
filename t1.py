23ee5d92fd6ebf9e27b613f7bde7bc195fa22601
Natsuhane-Ayari <tom3451617@gmail.com>
2024-01-05 10:59:35 +0800

    chimo0105

---
+++ t1.py
@@ -0,0 +1,9 @@
+class c1:
+    def __init__(self,name,id,chubanDate):
+        self._name = name
+        self._id = id
+        self._chubanDate = chubanDate
+
+a = c1("我推的孩子01",5,"2021/7/26")
+
+print(a._name)
