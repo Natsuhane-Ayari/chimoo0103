bca730129a4887d176028ae7fe86399b3ca4dd5e
Natsuhane-Ayari <tom3451617@gmail.com>
2024-01-05 11:37:03 +0800

    chimo0105

--- t1.py
+++ t1.py
@@ -1,9 +1,10 @@
 class c1:
+    def queryInfo(self):
+        print("書名: \t\t%s\nID: \t\t%s\n出版日期: \t%s"%(self._name,self._id,self._chubanDate))
     def __init__(self,name,id,chubanDate):
         self._name = name
         self._id = id
         self._chubanDate = chubanDate
 
 a = c1("我推的孩子01",5,"2021/7/26")
-
-print(a._name)
+a.queryInfo()
